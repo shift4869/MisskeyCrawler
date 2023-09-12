2b996a8224468ae9d996a0307cb8fd2a9cfe1570
shift <fantasy_world4423-git@yahoo.co.jp>
2023-09-12 15:46:06 +0900

    ミュータブルなデフォルト引数対策

--- misskeycrawler/Util.py
+++ misskeycrawler/Util.py
@@ -5,11 +5,14 @@ from typing import Any
 def find_values(obj: Any,
                 key: str,
                 is_predict_one: bool = False,
-                key_whitelist: list[str] = [],
-                key_blacklist: list[str] = []) -> Any | list[Any]:
+                key_whitelist: list[str] = None,
+                key_blacklist: list[str] = None) -> Any | list[Any]:
+    if not key_whitelist:
+        key_whitelist = []
+    if not key_blacklist:
+        key_blacklist = []
+
     def _inner_helper(inner_obj: Any, inner_key: str, inner_result: list) -> list:
-        # if inner_key in key_blacklist:
-        #     return inner_result
         if isinstance(inner_obj, dict) and (inner_dict := inner_obj):
             for k, v in inner_dict.items():
                 if k == inner_key:
